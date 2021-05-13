8e31c23033fdce5f8eb9e65451f3f36593eaed25
tanimasharma <tanimasharma@gmail.com>
2021-05-13 10:42:57 +0530

    first commit

---
+++ ASCII.py
@@ -0,0 +1,7 @@
+#PYTHON,INDIA
+string=input("enter string:")
+for i in range(0,len(string)):
+    for j in range(0,i+1):
+        print(string[j],end=' ')
+    print()
+       
---
+++ avgerage in a list.py
@@ -0,0 +1,6 @@
+x=[]
+for i in range(1,16):
+    n=int(input("enter the number"))
+    x.append(n)
+print(sum(x))
+print(sum(x)/len(x))
